96eb3e92ea5d697a459c18d060e7df5f40624f3e
Robert Joachim Encinas <robert_joachim_encinas@dlsu.edu.ph>
2023-02-18 16:25:23 +0800

    heuristic functions and generator for all nodes

---
+++ heuristic.py
@@ -0,0 +1,13 @@
+import math
+
+def heuristicFunction(node, end):
+    h_value = math.abs(node.position.x - end.position.x) + math.abs(node.position.y - end.position.y)
+    
+    if len(node.connections) == 1:
+        correctionFactor = node.connections[0].cost
+
+    return h_value + correctionFactor
+
+def determineHeuristicValues(graph):
+    for vertex in graph.vertices.keys():
+        graph.vertices[vertex] = heuristicFunction(vertex, graph.val)
