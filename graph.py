bb3ad72eae90ee019f76a0a85d9acaa5815bf0de
nemurinoU <63938840+nemurinoU@users.noreply.github.com>
2023-02-25 17:03:33 +0800

    Rob's Code

---
+++ graph.py
@@ -0,0 +1,88 @@
+from enum import Enum
+
+class Graph:
+    def __init__ (self, edges, vertexSet, start, end):
+        #set of all edges
+        self.edges = edges
+        #dict where the key is the Node and the value is the heuristic score
+        self.vertices = dict()
+        #set of vertices
+        self.vertexSet = vertexSet
+        #starting node
+        self.start = start
+        #ending node
+        self.end = end
+            
+    def print_graph (self):
+        for k in sorted(list(self.vertices.keys())):
+            print (k + str(self.vertices[k].connections))
+
+class NodeType (Enum):
+    START = 0
+    TERMINAL = 1
+    EDGE_NODE = 2
+    JUNCTION = 3
+    WALL = 4
+    DEAD_END = 5
+
+class Coordinate:
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
+
+class Node:
+    def __init__ (self, id, position, nodeType):
+        #self.id refers to the numerical code (index) of the node in the adjacency matrix
+        self.id = id
+        #self.position is the Coordinate of the Node
+        self.position = position
+        #self.connections would be an array of Edges
+        self.connections = [] 
+        #self.adjacents would be an array of Coordinates
+        self.adjacents = []
+        #self.type identifies if the node is the starting node, ending node, or just a regular node
+        self.type = nodeType
+        
+    def add_connection (self, edge):
+        if edge not in self.connections:
+            self.connections.append (edge)
+
+class Line:
+    def __init__ (self, start, end):
+        #self.start is the Coordinate of the starting point of the line
+        self.start = start
+        #self.end is the Coordinate of the ending point of the line
+        self.end = end
+
+class Edge:
+    def __init__ (self):
+        #self.nodes is a 2 element array of the 2 Nodes that the edge joins together
+        self.nodes = [-1, -1]
+        #self.lines is the array of lines that makes up the edge (Note: Lines must be in correct traversal order)
+        self.lines = []
+        #self.cost is the cost to traverse this Edge
+        self.cost = 0
+
+    def addLines (self, line):
+        self.lines.append(line)
+
+    def getCost (self):
+        for line in self.lines:
+            self.cost += abs(line.start.x - line.end.x) + abs(line.start.y - line.end.y)
+            self.cost += 1
+        self.cost += 1
+
+#The dot that traverses the graph
+class Pointer:
+    def __init__ (self, val, position):
+        self.data = val
+        #self.position is the Coordinate of where the Pointer is currently at
+        self.position = position
+
+    def traverse (self, edge):
+        if self.position == edge.nodes[0]:
+            self.position = edge.nodes[1]
+        else:
+            self.position = edge.nodes[0]
+
+        
---
+++ textToGraph.py
@@ -0,0 +1,115 @@
+from graph import *
+from heuristic import *
+
+def hasTurn(line, next):
+    if line.start.x - next.x != 0 and line.start.y - next.y != 0:
+        return True
+    else:
+        return False
+
+def defineEdges(junctionList, nodeGrid):
+    edges = []
+    currentLine = None
+
+    for j in junctionList:
+        for coord in j.adjacents:
+            currentEdge = Edge()
+            currentLine = Line(coord, coord)
+            previous = j.position
+            next = coord
+            currentEdge.nodes[0] = nodeGrid[previous.x][previous.y]
+            
+            end = False
+            deadEnd = False
+            while not end:
+                if nodeGrid[next.x][next.y].type != NodeType.EDGE_NODE:
+                    if nodeGrid[next.x][next.y].type == NodeType.DEAD_END:
+                        deadEnd = True
+
+                    currentEdge.nodes[1] = nodeGrid[next.x][next.y]
+                    currentEdge.addLines(currentLine)
+                    end = True
+                else:
+                    if hasTurn(currentLine, next):
+                        currentEdge.addLines(currentLine)
+                        currentLine = Line(next, next)
+                    else:
+                        currentLine.end = next
+
+                if not end:
+                    if len(nodeGrid[next.x][next.y].adjacents) == 1:
+                        previous = next
+                        next = nodeGrid[next.x][next.y].adjacents[0]
+                    elif nodeGrid[next.x][next.y].adjacents[0].x == previous.x and nodeGrid[next.x][next.y].adjacents[0].y == previous.y:
+                        previous = next
+                        next = nodeGrid[next.x][next.y].adjacents[1]
+                    else:
+                        previous = next
+                        next = nodeGrid[next.x][next.y].adjacents[0]
+            
+            if not deadEnd:
+                currentEdge.getCost()
+                j.connections.append(currentEdge)
+                edges.append(currentEdge)
+
+    return edges
+
+#The grid to graph parser transforms the grid to a graph representation 
+def gridToGraph(n, grid):
+    currId = 1
+    junctionList = []
+    nodeGrid = []
+
+    for x in range(n):
+        nodeGrid.append([])
+        for y in range(n):
+            if grid[x][y] == "S":
+                nodeGrid[x].append(Node(0, Coordinate(x, y), NodeType.START))
+                start = nodeGrid[x][y]
+            elif grid[x][y] == "G":
+                nodeGrid[x].append(Node(-1, Coordinate(x, y), NodeType.TERMINAL))
+                end = nodeGrid[x][y]
+            elif grid[x][y] == ".":
+                nodeGrid[x].append(Node(-10, Coordinate(x, y), NodeType.EDGE_NODE))
+            else:
+                nodeGrid[x].append(Node(-10, Coordinate(x, y), NodeType.WALL))
+
+            #Process node
+            if nodeGrid[x][y].type != NodeType.WALL:
+                #Check up
+                if y - 1 >= 0:
+                    if grid[x][y - 1] != "#":
+                        nodeGrid[x][y].adjacents.append(Coordinate(x, y - 1))
+                
+                #Check down
+                if y + 1 < n:
+                    if grid[x][y + 1] != "#":
+                        nodeGrid[x][y].adjacents.append(Coordinate(x, y + 1))
+
+                #Check left
+                if x - 1 >= 0:
+                    if grid[x - 1][y] != "#":
+                        nodeGrid[x][y].adjacents.append(Coordinate(x - 1, y))
+                
+                #Check down
+                if x + 1 < n:
+                    if grid[x + 1][y] != "#":
+                        nodeGrid[x][y].adjacents.append(Coordinate(x + 1, y))
+                
+                if len(nodeGrid[x][y].adjacents) > 2 or nodeGrid[x][y].type == NodeType.START or nodeGrid[x][y].type == NodeType.TERMINAL:
+                    nodeGrid[x][y].id = currId
+
+                    if nodeGrid[x][y].type == NodeType.EDGE_NODE:
+                        nodeGrid[x][y].type = NodeType.JUNCTION
+
+                    junctionList.append(nodeGrid[x][y])
+                    currId += 1
+                elif len(nodeGrid[x][y].adjacents) == 1:
+                    nodeGrid[x][y].type = NodeType.DEAD_END
+
+    edges = defineEdges(junctionList, nodeGrid)
+
+    g = Graph(edges, junctionList, start, end)
+    determineHeuristicValues(g)
+
+    return g
