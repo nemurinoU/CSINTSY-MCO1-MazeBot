711e1c079c919d0a8231369cd88f4927dd703d2f
nemurinoU <63938840+nemurinoU@users.noreply.github.com>
2023-03-06 16:33:42 +0800

    Update launch.bat

--- app/launch.bat
+++ app/launch.bat
@@ -3,22 +3,22 @@
 
 python ../source/main.py > nul 2> nul
 if  errorlevel 9009 goto TRY_1
-else goto SUCCESS
+goto SUCCESS
 
 :TRY_1
 py3 ../source/main.py > nul 2> nul
-if  errorlevel 9009 goto TRY_2
-else goto SUCCESS
+if  errorlevel 9009 goto TRY_2 
+goto SUCCESS
 
 :TRY_2
 python3 ../source/main.py > nul 2> nul
-if  errorlevel 9009 goto TRY_3
-else goto SUCCESS
+if  errorlevel 9009 goto TRY_3 
+goto SUCCESS
 
 :TRY_3
 py ../source/main.py > nul 2> nul
-if  errorlevel 9009 goto ERROR
-else goto SUCCESS
+if  errorlevel 9009 goto ERROR 
+goto SUCCESS
 
 :SUCCESS
 echo SUCCESSFUL
@@ -31,4 +31,3 @@ cmd /k
 exit /b 1
 
 :EOF 
-
